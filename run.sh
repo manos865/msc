99890174e1dc4ecceb425f8d03e724bb1282948a
Manolis Androulidakis <manosandroulidakis31@gmail.com>
2021-03-01 01:36:19 +0200

    Add run.sh script and output.txt

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for size in 5 10 25 50 100 250 500 750 1000 2500 5000
+for size in 5 10 25 50 100 250 500 1000 2500
 do
-    for conn in 0.01 0.025 0.05 0.10 0.25 0.5 0.75 0.90
+    for conn in 0.05 0.10 0.25 0.5 0.75
     do
         python3 detect.py $size $conn
     done
